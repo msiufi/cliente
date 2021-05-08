c1587fc2852213cef9702599cc029863f73deeb0
Maximiliano Siufi <maxisiufi@gmail.com>
2021-05-08 12:38:13 -0300

    primeros pasos

---
+++ src/components/auth/Login.js
@@ -0,0 +1,13 @@
+
+
+const Login = () => {
+  return ( 
+      <div className="form-usuario">
+        <div className="contenedor-sorm sombra-dark">
+          <h1> </h1>
+        </div>
+      </div>
+   );
+}
+ 
+export default Login;
---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NuevaCuenta = () => {
+  return ( 
+    <h1>DesdeNuevaCuenta</h1>
+   );
+}
+ 
+export default NuevaCuenta;
