df69e0f7beba8459ac4f37a3087f298cd3e22488
Maximiliano Siufi <maxisiufi@gmail.com>
2021-05-09 20:14:59 -0300

    Listado de tareas + tareas + botones y tags

---
+++ src/components/tareas/FormTarea.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const FormTarea = () => {
+  return ( 
+      <div className="formulario">
+    <form>
+      <div className="contenedor-input">
+        <input type="text"
+        className="input-text"
+        placeholder="NombreTarea..."
+        name="nombre" />
+      </div>
+
+      <div className="contenedor-input">
+        <input type="submit"
+        className="btn btn-primario btn-submit btn-block"
+        value="Agregar tarea" />
+      </div>
+    </form>
+    </div>
+   );
+}
+ 
+export default FormTarea;
---
+++ src/components/tareas/Tarea.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Tarea = ({tarea}) => {
+  return ( 
+
+    <li className="tarea sombra">
+
+      <p>{tarea.nombre}</p>
+
+      <div className="estado">
+        {tarea.estado
+        ?
+          ( 
+            <button
+              type="button"
+              className="completo"
+            >Completo</button>
+
+          )
+        :
+          ( 
+            <button
+              type="button"
+              className="Incompleto"
+            >Incompleto</button>
+
+          )
+        }
+      </div>
+
+      <div className="acciones">
+        <button
+        type="button"
+        className="btn btn-secundario"
+        >Editar</button>
+        
+        <button 
+          type="button"
+          className="btn btn-primario"
+        >Eliminar</button>
+      </div>
+
+    </li>
+   );
+}
+ 
+export default Tarea;
