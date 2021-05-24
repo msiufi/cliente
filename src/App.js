507b08517e2dcc44f462fee10070bcb9a731b5c9
Maximiliano Siufi <maxisiufi@gmail.com>
2021-05-24 13:01:43 -0300

    Creating alerts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/auth/Login'
 import NuevaCuenta from './components/auth/NuevaCuenta'
 import Proyectos from './components/proyectos/Proyectos'
 
 import ProyectoState from './context/proyectos/proyectoState'
 import TareaState from './context/tareas/tareaState'
+import AlertaState from './context/alertas/alertaState'
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
   <ProyectoState>
     <TareaState>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-          <Route exact path="/proyectos" component={Proyectos} />
-        </Switch>
-      </Router>
+      <AlertaState>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+            <Route exact path="/proyectos" component={Proyectos} />
+          </Switch>
+        </Router>
+      </AlertaState>
     </TareaState>
   </ProyectoState>
 
